7f596797cf1d0c5387ef3206264819b23a1fae3d
Sonnyducks <sonnyducks@yahoo.com>
2016-05-22 17:14:46 -0500

    Update Start and End Date Validation.js

--- Start and End Date Validation.js
+++ Start and End Date Validation.js
@@ -1,3 +1,12 @@
+/*
+Business Rule: 
+Table: Change Request 
+When: Before 
+Insert - true 
+Update - true 
+Condition - current.work_start.changes() || current.work_end.changes()
+*/
+
 if ((!current.work_start.nil()) && (!current.work_end.nil())) { 
 	var start = current.work_start.getGlideObject().getNumericValue(); 
 	var end = current.work_end.getGlideObject().getNumericValue(); 
