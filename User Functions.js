1f3fb4c47f272004e600ae8ef26162f96162cce1
Sonnyducks <sonnyducks@yahoo.com>
2016-05-22 13:20:33 -0500

    Create User Functions.js

---
+++ User Functions.js
@@ -0,0 +1,12 @@
+gs.getUserName();
+#Return: craig.granier 
+ 
+gs.getUserID();
+#Return: 678a93186f56010097d789f1be3ee4da 
+ 
+gs.getUserDisplayName();
+#Return: Craig Granier 
+ 
+gs.getUser();
+#Return:com.glide.sys.User@1959acb (this is what a object looks like) 
+ 
