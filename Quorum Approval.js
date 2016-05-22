fa003359af2e3d23f118bd63b70b69308fcbf8e4
Sonnyducks <sonnyducks@yahoo.com>
2016-05-22 18:50:26 -0500

    Create Quorum Approval.js

---
+++ Quorum Approval.js
@@ -0,0 +1,10 @@
+var percentApproved = counts.approved / counts.total * 100; 
+var percentRejected = counts.rejected / counts.total * 100; 
+var percentResponded = (counts.approved + counts.rejected) / counts.total * 100; 
+if (percentResponded > 50) { 
+if (percentApproved > 50) 
+answer = 'approved'; 
+ 
+if (percentRejected > 50) 
+answer = 'rejected'; 
+}
