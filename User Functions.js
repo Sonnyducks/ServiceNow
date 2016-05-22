b84a44d79cc2df9da8c35472f3ac3a914104b782
Sonnyducks <sonnyducks@yahoo.com>
2016-05-22 13:21:22 -0500

    Update User Functions.js

--- User Functions.js
+++ User Functions.js
@@ -1,12 +1,12 @@
 gs.getUserName();
-#Return: craig.granier 
+//Return: craig.granier 
  
 gs.getUserID();
-#Return: 678a93186f56010097d789f1be3ee4da 
+//Return: 678a93186f56010097d789f1be3ee4da 
  
 gs.getUserDisplayName();
-#Return: Craig Granier 
+//Return: Craig Granier 
  
 gs.getUser();
-#Return:com.glide.sys.User@1959acb (this is what a object looks like) 
+//Return:com.glide.sys.User@1959acb (this is what a object looks like) 
  
