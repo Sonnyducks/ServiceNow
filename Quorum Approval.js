67da4e63c76555e8d1944787e3136817047474a6
Sonnyducks <sonnyducks@yahoo.com>
2016-05-22 18:52:06 -0500

    Update Quorum Approval.js

--- Quorum Approval.js
+++ Quorum Approval.js
@@ -1,10 +1,12 @@
+//USE:  Workflow approval activity script
+
 var percentApproved = counts.approved / counts.total * 100; 
 var percentRejected = counts.rejected / counts.total * 100; 
 var percentResponded = (counts.approved + counts.rejected) / counts.total * 100; 
 if (percentResponded > 50) { 
 if (percentApproved > 50) 
-answer = 'approved'; 
+ answer = 'approved'; 
  
 if (percentRejected > 50) 
-answer = 'rejected'; 
+ answer = 'rejected'; 
 }
