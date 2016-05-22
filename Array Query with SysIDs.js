80f28ebe507e75375f9228529865d69608cca81c
Sonnyducks <sonnyducks@yahoo.com>
2016-05-22 18:31:35 -0500

    Update Array Query with SysIDs.js

--- Array Query with SysIDs.js
+++ Array Query with SysIDs.js
@@ -3,12 +3,12 @@
 var arrayUtil = new ArrayUtil(); 
 var locIDs = new Array(''); 
 var i = 0; 
-var co = new GlideRecord('service_commitment'); 
-co.addQuery('u_sps_po_number', parent.u_sps_contract_number); 
+var co = new GlideRecord('service_contract'); 
+co.addQuery('u_po_number', parent.u_contract_number); 
 co.query(); 
 while (co.next()) { 
-	if (arrayUtil.indexOf(locIDs, co.u_sps_location.sys_id) == -1){ 
-		locIDs[i] = co.u_sps_location.sys_id; 
+	if (arrayUtil.indexOf(locIDs, co.u_location.sys_id) == -1){ 
+		locIDs[i] = co.u_location.sys_id; 
 		i++; 
 	} 
 }
