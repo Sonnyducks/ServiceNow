b95a6080aada937badbb2e3b4a359818d9dfc6fc
Sonnyducks <sonnyducks@yahoo.com>
2016-05-22 13:26:02 -0500

    Update Controlling Tabs.js

--- Controlling Tabs.js
+++ Controlling Tabs.js
@@ -1,6 +1,7 @@
-//Tab Focus 
+//Use:  Client Scripts
+//Tab Focus, '1' is the number of the tab you want active
 g_tabs2Sections.setActive(1);
 
-//Hide a Tab (section) 
+//Hide a Tab (section), replace '<tab_name>' with the name of the tab you want to hide
 var section = $$('span[tab_caption="<tab_name>"]')[0].select('span[id*=section.]')[0]; 
 section.hide();
