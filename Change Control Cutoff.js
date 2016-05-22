f59cf39b4d23c492c62c399169764bcabe552278
Sonnyducks <sonnyducks@yahoo.com>
2016-05-22 18:58:51 -0500

    Update Change Control Cutoff.js

--- Change Control Cutoff.js
+++ Change Control Cutoff.js
@@ -7,26 +7,24 @@ var changeCutoff = getChangeCutoffDate(submit);
 gs.print('Change cutoff time is ' + changeCutoff); 
  
 if (submit <= changeCutoff && planStart > changeCutoff) { 
-gs.print('Change is OK'); 
+ gs.print('Change is OK'); 
 } else { 
-gs.print('Change is too late'); 
+ gs.print('Change is too late'); 
 } 
  
 function getChangeCutoffDate(d) { 
-var changeCutoffDay = 3;  //1=Monday...7=Sunday 
-var returnDate = new GlideDateTime(d); 
-    var day = returnDate.getDayOfWeekLocalTime(); 
-if (day == 7) {day = 0;}   //if Sunday set to 0 instead of 7 
-gs.print('Change start day of the week is ' + day); 
+ var changeCutoffDay = 3;  //1=Monday...7=Sunday 
+ var returnDate = new GlideDateTime(d); 
+ var day = returnDate.getDayOfWeekLocalTime(); 
+ if (day == 7) {day = 0;}   //if Sunday set to 0 instead of 7 
+  gs.print('Change start day of the week is ' + day); 
  
-if (day <= changeCutoffDay ) { 
-    return returnDate.getInternalMidnight(changeCutoffDay); 
-} 
+ if (day <= changeCutoffDay ) { 
+  return returnDate.getInternalMidnight(changeCutoffDay); 
+ } 
  
-if (day > changeCutoffDay) { 
-returnDate.addWeeksLocalTime(1); 
-    return returnDate.getInternalMidnight(changeCutoffDay); 
-} 
- 
- 
+ if (day > changeCutoffDay) { 
+  returnDate.addWeeksLocalTime(1); 
+  return returnDate.getInternalMidnight(changeCutoffDay); 
+ } 
 } 
