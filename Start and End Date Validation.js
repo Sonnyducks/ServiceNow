bca0442fa12224bc59ab89a4dec8914742624bbb
Sonnyducks <sonnyducks@yahoo.com>
2016-05-22 17:13:26 -0500

    Update Start and End Date Validation.js

--- Start and End Date Validation.js
+++ Start and End Date Validation.js
@@ -1,10 +1,10 @@
 if ((!current.work_start.nil()) && (!current.work_end.nil())) { 
-    var start = current.work_start.getGlideObject().getNumericValue(); 
-    var end = current.work_end.getGlideObject().getNumericValue(); 
-    if (start > end) { 
-  current.work_start.setError('start must be before end'); 
-  current.work_end.setError('start must be before end'); 
-        gs.addErrorMessage('Work Start date must be before Work End date'); 
-        current.setAbortAction(true); 
-    } 
+	var start = current.work_start.getGlideObject().getNumericValue(); 
+	var end = current.work_end.getGlideObject().getNumericValue(); 
+	if (start > end) { 
+		current.work_start.setError('start must be before end'); 
+		current.work_end.setError('start must be before end'); 
+		gs.addErrorMessage('Work Start date must be before Work End date'); 
+		current.setAbortAction(true); 
+	} 
 }
