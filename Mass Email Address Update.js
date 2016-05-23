1265e04159db21869f70a938006d2d8ffeaf7bcc
Sonnyducks <sonnyducks@yahoo.com>
2016-05-22 19:09:58 -0500

    Create Mass Email Address Update.js

---
+++ Mass Email Address Update.js
@@ -0,0 +1,12 @@
+//Use: Fix script, to update all email domains in the user table 
+ 
+var usr = new GlideRecord('sys_user'); 
+usr.addEncodedQuery('emailLIKEbad_domain.com'); 
+usr.query(); 
+while (usr.next()) { 
+  //gs.print('OLD EMAIL: ' + usr.email); 
+  var r  = new SNC.Regex('/bad_domain/'); 
+  usr.email = r.replaceAll(usr.email, 'new_domain'); 
+  usr.update(); 
+  //gs.print('NEW EMAIL: ' + usr.email); 
+}
