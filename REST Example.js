52b9cc1a8e4c664f75c54c52ff9f3d2aed77b83f
Sonnyducks <sonnyducks@yahoo.com>
2016-05-27 07:24:54 -0500

    Create REST Example.js

---
+++ REST Example.js
@@ -0,0 +1,14 @@
+var refNumber = 'INC0000001';
+var request = new sn_ws.RESTMessageV2();
+request.setEndpoint('https://mcguirewoodsdev.service-now.com/api/now/table/incident?sysparm_query=&sysparm_limit=1&number=' + refNumber);
+request.setHttpMethod('GET');
+
+//Eg. UserName="admin", Password="admin" for this code sample.  *NEEDS UPDATING
+var user = 'admin';
+var password = 'admin';
+
+request.setBasicAuth(user,password);
+request.setRequestHeader("Accept","application/json");
+ 
+var response = request.execute();
+gs.log(response.getBody());
