003a27f59f3c4387b318d0c3fc6cd8c14e126d40
Sonnyducks <sonnyducks@yahoo.com>
2016-06-10 09:57:22 -0500

    Update REST Example.js

--- REST Example.js
+++ REST Example.js
@@ -1,6 +1,6 @@
 var refNumber = 'INC0000001';
 var request = new sn_ws.RESTMessageV2();
-request.setEndpoint('https://mcguirewoodsdev.service-now.com/api/now/table/incident?sysparm_query=&sysparm_limit=1&number=' + refNumber);
+request.setEndpoint('https://YOUR_INSTANCE.service-now.com/api/now/table/incident?sysparm_query=&sysparm_limit=1&number=' + refNumber);
 request.setHttpMethod('GET');
 
 //Eg. UserName="admin", Password="admin" for this code sample.  *NEEDS UPDATING
