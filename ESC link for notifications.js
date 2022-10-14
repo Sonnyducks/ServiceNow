2e8560b1fe22d238127cf1b7e034dfe9e2ff7f8a
Sonnyducks <sonnyducks@yahoo.com>
2022-10-14 08:47:29 -0500

    Create ESC link for notifications.js

---
+++ ESC link for notifications.js
@@ -0,0 +1,11 @@
+(function runMailScript(/* GlideRecord */ current, /* TemplatePrinter */ template,
+
+/* Optional EmailOutbound */ email, /* Optional GlideRecord */ email_action,
+
+/* Optional GlideRecord */ event) {
+
+  var url = '<a href="' + gs.getProperty('glide.servlet.uri') + 'esc?id=hrm_ticket_page&table=' + current.sys_class_name + '&sys_id=' + current.sys_id + '">Link</a>';
+
+  template.print(url);
+
+})(current, template, email, email_action, event);
